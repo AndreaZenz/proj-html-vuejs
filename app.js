74f3d884d6d1a5d66c06b20e0e4b46dce1c8d087
AndreaZenz <zenzero.ere@gmail.com>
2021-05-24 15:17:25 +0200

    navbar half completed

---
+++ app.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: "#app",
+    data: {
+        navbarLinks: [{
+            text: "HOME",
+            link: "#"
+        }, {
+            text: "ELEMENT",
+            link: "#"
+        }, {
+            text: "FEATURES",
+            link: "#"
+        }, {
+            text: "PAGES",
+            link: "#"
+        }, {
+            text: "PORTFOLIO",
+            link: "#"
+        }, {
+            text: "BLOG",
+            link: "#"
+        }, {
+            text: "SHOP",
+            link: "#"
+        },],
+        footerData: {
+            // twitterData: [
+            //     {
+            //         text: "lorem"
+            //     }, {
+            //         text: "lorem"
+            //     },
+            // ],
+            // links:[
+                
+            // ]
+
+        }
+
+
+
+    }
+})
